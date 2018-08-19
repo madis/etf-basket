a4ed61e35bfd88aece95adafbf214300e873c019
Madis Nõmme <me@mad.is>
2018-08-19 15:35:42 +0300

    Add simple root route

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 feature 'Landing page' do
   it 'welcomes user' do
     visit '/'
-    expect(page).to have_content 'Welcome to speedrail'
+    expect(page).to have_content 'Welcome to Etf Basket App'
   end
 end
