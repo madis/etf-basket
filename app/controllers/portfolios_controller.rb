cbb61bd2826207146c0836f7c0626e946ac8dbe5
Madis Nõmme <me@mad.is>
2018-08-22 10:03:04 +0300

    Allow adding multiple ETF-s to portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,28 +1,20 @@
 class PortfoliosController < ApplicationController
   before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
 
-  # GET /portfolios
-  # GET /portfolios.json
   def index
     @portfolios = Portfolio.all
   end
 
-  # GET /portfolios/1
-  # GET /portfolios/1.json
   def show
   end
 
-  # GET /portfolios/new
   def new
     @portfolio = Portfolio.new
   end
 
-  # GET /portfolios/1/edit
   def edit
   end
 
-  # POST /portfolios
-  # POST /portfolios.json
   def create
     @portfolio = Portfolio.new(portfolio_params)
 
@@ -37,8 +29,6 @@ class PortfoliosController < ApplicationController
     end
   end
 
-  # PATCH/PUT /portfolios/1
-  # PATCH/PUT /portfolios/1.json
   def update
     respond_to do |format|
       if @portfolio.update(portfolio_params)
@@ -51,8 +41,6 @@ class PortfoliosController < ApplicationController
     end
   end
 
-  # DELETE /portfolios/1
-  # DELETE /portfolios/1.json
   def destroy
     @portfolio.destroy
     respond_to do |format|
@@ -69,6 +57,6 @@ class PortfoliosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def portfolio_params
-      params.require(:portfolio).permit(:name, :etf_ids)
+      params.require(:portfolio).permit(:name, etf_ids: [])
     end
 end
