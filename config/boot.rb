0c1366427cb2287c214780500ab0b10e5f8e8417
Madis Nõmme <me@mad.is>
2018-08-29 09:26:50 +0300

    Deploy with capistrano

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,7 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+if ENV['RAILS_ENV'] != 'production'
+  # Disable bootsnap until https://forum.snapcraft.io/t/ld-library-path-in-classic-snap/5030/32
+  require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+end
